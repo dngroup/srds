db3e3441a4de784ee9878f3c525b40e666a6a7be
mlacaud <matlaca24@gmail.com>
2018-03-06 00:16:16 +0100

    port missing management + readme

--- srdsserver/main.cpp
+++ srdsserver/main.cpp
@@ -6,13 +6,19 @@
 
 int main(int argc, char ** argv) {
     char * arg;
+    int port;
+    if (argc != 2) {
+        port = 8080;
+    } else {
+        port = atoi(argv[1]);
+    }
 
     for (int i = 1; i <= argc; i++) {
         arg = argv[i];
         std::cout << arg << std::endl;
     }
 
-    startServer(atoi(argv[1]));
+    startServer(port);
 
     return 0;
 }
