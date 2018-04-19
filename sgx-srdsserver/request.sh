1495f3b678fa583e9315f7f39ea1e547b28d9ae1
Simon Da Silva <contact@simondasilva.fr>
2018-04-19 20:16:52 +0200

    Request

--- sgx-srdsserver/request.sh
+++ sgx-srdsserver/request.sh
@@ -2,3 +2,5 @@ wget http://msstream.net:8023/api/description/ANxeFQQ89Vg/1?order=111111000000&q
 wget http://msstream.net:8023/api/description/ANxeFQQ89Vg/6111175/init
 http://localhost:8080/api/description/ANxeFQQ89Vg/1?order=111111000000&q0=412194&q1=6111175
 http://localhost:8080/api/description/ANxeFQQ89Vg/6111175/init
+curl -XGET 147.210.128.146:8082/api/description/-VTTEQN_Q1o/413240/init -H "X-Forwarded-Host: 147.210.128.140:8081"
+
