30e5a968220c9e318af680cb970b94e99ecbc723
Simon Da Silva <contact@simondasilva.fr>
2018-05-09 19:53:45 +0200

    Scripts update

--- sgx-srdsserver/setup-env.sh
+++ sgx-srdsserver/setup-env.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 
 cd ~
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt update
+sudo apt install -y git firefox nodejs curl
+sudo npm -g install grunt
+sudo npm -g install grunt-cli
+sudo npm -g install http-server
+
+cd ~
+curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+cd ~
+mkdir content
+
+cd ~
+git clone https://github.com/dngroup/msstream-player.git player
+cd ~/player
+git checkout dev_srds
+npm install
+grunt
+
+cd ~
 git clone https://github.com/dngroup/srds.git srds
-cd srds
-git pull
-cd sgx-srdsserver
+cd ~/srds/sgx-srdsserver
 make clean
 make
 
--- sgx-srdsserver/start-client.sh
+++ sgx-srdsserver/start-client.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
+cd ~/content
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+sudo docker run -d -p 8080:8080 -v $(pwd):/src/main/ressources msstream/server_srds
+
+cd ~/player
 git pull
+git checkout dev_srds
+grunt
+http-server --cors -p 8082 &
+
+cd ~/srds/sgx-srdsserver
 make clean
+git pull
 make
 ./SGXSRDSServerApp 8081 0 &
+
 firefox http://localhost:8082/samples/dash-if-reference-player/index.html
-killall SGXSRDSServerApp
+
 killall firefox
+killall SGXSRDSServerApp
+killall http-server
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
 
