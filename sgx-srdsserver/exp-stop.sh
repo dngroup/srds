3015952d5349f0a0ace7f6c71eaf0b9bcc60769c
Simon Da Silva <contact@simondasilva.fr>
2018-05-14 18:44:40 +0200

    exp

--- sgx-srdsserver/exp-stop.sh
+++ sgx-srdsserver/exp-stop.sh
@@ -2,15 +2,15 @@
 
 set -x
 
-ssh nuc@147.210.129.172 'nohup ~/srds/sgx-srdsserver/stop-client.sh &'
-ssh nuc@147.210.129.119 'nohup ~/srds/sgx-srdsserver/stop-client.sh &'
-ssh nuc@147.210.128.61 'nohup ~/srds/sgx-srdsserver/stop-client.sh &'
-ssh nuc@147.210.129.156 'nohup ~/srds/sgx-srdsserver/stop-client.sh &'
-ssh nuc@147.210.128.135 'nohup ~/srds/sgx-srdsserver/stop-client.sh &'
+ssh nuc@147.210.129.172 '~/srds/sgx-srdsserver/stop-client.sh'
+ssh nuc@147.210.129.119 '~/srds/sgx-srdsserver/stop-client.sh'
+ssh nuc@147.210.128.61 '~/srds/sgx-srdsserver/stop-client.sh'
+ssh nuc@147.210.129.156 '~/srds/sgx-srdsserver/stop-client.sh'
+ssh nuc@147.210.128.135 '~/srds/sgx-srdsserver/stop-client.sh'
 
 sleep 1
 
-ssh nuc@147.210.129.241 'nohup ~/srds/sgx-srdsserver/stop-client.sh &'
+ssh nuc@147.210.129.241 '~/srds/sgx-srdsserver/stop-client.sh'
 
 sleep 1
 
