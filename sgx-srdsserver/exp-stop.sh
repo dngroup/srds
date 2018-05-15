a4a9df488192ef67b45cda301c225156aa6c6d0c
Simon Da Silva <contact@simondasilva.fr>
2018-05-15 14:45:19 +0200

    disown

--- sgx-srdsserver/exp-stop.sh
+++ sgx-srdsserver/exp-stop.sh
@@ -2,15 +2,15 @@
 
 set -x
 
-ssh nuc@147.210.129.172 '~/srds/sgx-srdsserver/stop-client.sh'
-ssh nuc@147.210.129.119 '~/srds/sgx-srdsserver/stop-client.sh'
-ssh nuc@147.210.128.61 '~/srds/sgx-srdsserver/stop-client.sh'
-ssh nuc@147.210.129.156 '~/srds/sgx-srdsserver/stop-client.sh'
-ssh nuc@147.210.128.135 '~/srds/sgx-srdsserver/stop-client.sh'
+ssh nuc@147.210.129.172 '~/srds/sgx-srdsserver/stop-client.sh && disown -a &'
+ssh nuc@147.210.129.119 '~/srds/sgx-srdsserver/stop-client.sh && disown -a &'
+ssh nuc@147.210.128.61 '~/srds/sgx-srdsserver/stop-client.sh && disown -a &'
+ssh nuc@147.210.129.156 '~/srds/sgx-srdsserver/stop-client.sh && disown -a &'
+ssh nuc@147.210.128.135 '~/srds/sgx-srdsserver/stop-client.sh && disown -a &'
 
 sleep 1
 
-ssh nuc@147.210.129.241 '~/srds/sgx-srdsserver/stop-client.sh'
+ssh nuc@147.210.129.241 '~/srds/sgx-srdsserver/stop-client.sh && disown -a &'
 
 sleep 1
 
