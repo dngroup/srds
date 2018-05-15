ea864bb2e79c0fb678f86eebee3cacfea2307ffb
Simon Da Silva <contact@simondasilva.fr>
2018-05-15 14:55:28 +0200

    screen

--- sgx-srdsserver/exp-update-all.sh
+++ sgx-srdsserver/exp-update-all.sh
@@ -6,12 +6,12 @@ source exp-stop.sh
 
 sleep 10
 
-ssh nuc@147.210.129.172 'cd ~/srds/sgx-srdsserver/ && git pull && cd ~/player && git pull && git checkout dev_srds && grunt && disown -a &'
-ssh nuc@147.210.129.119 'cd ~/srds/sgx-srdsserver/ && git pull && cd ~/player && git pull && git checkout dev_srds && grunt && disown -a &'
-ssh nuc@147.210.128.61 'cd ~/srds/sgx-srdsserver/ && git pull && cd ~/player && git pull && git checkout dev_srds && grunt && disown -a &'
-ssh nuc@147.210.129.156 'cd ~/srds/sgx-srdsserver/ && git pull && cd ~/player && git pull && git checkout dev_srds && grunt && disown -a &'
-ssh nuc@147.210.128.135 'cd ~/srds/sgx-srdsserver/ && git pull && cd ~/player && git pull && git checkout dev_srds && grunt && disown -a &'
-ssh nuc@147.210.129.241 'cd ~/srds/sgx-srdsserver/ && git pull && cd ~/player && git pull && git checkout dev_srds && grunt && disown -a &'
+ssh nuc@147.210.129.172 'screen -d -m cd ~/srds/sgx-srdsserver/ && git pull && cd ~/player && git pull && git checkout dev_srds && grunt'
+ssh nuc@147.210.129.119 'screen -d -m cd ~/srds/sgx-srdsserver/ && git pull && cd ~/player && git pull && git checkout dev_srds && grunt'
+ssh nuc@147.210.128.61 'screen -d -m cd ~/srds/sgx-srdsserver/ && git pull && cd ~/player && git pull && git checkout dev_srds && grunt'
+ssh nuc@147.210.129.156 'screen -d -m cd ~/srds/sgx-srdsserver/ && git pull && cd ~/player && git pull && git checkout dev_srds && grunt'
+ssh nuc@147.210.128.135 'screen -d -m cd ~/srds/sgx-srdsserver/ && git pull && cd ~/player && git pull && git checkout dev_srds && grunt'
+ssh nuc@147.210.129.241 'screen -d -m cd ~/srds/sgx-srdsserver/ && git pull && cd ~/player && git pull && git checkout dev_srds && grunt'
 
 ssh -t nuc@147.210.129.172 'cd ~/Downloads/linux-sgx-driver/ && make clean && make && sudo mkdir -p "/lib/modules/"`uname -r`"/kernel/drivers/intel/sgx" && sudo cp isgx.ko "/lib/modules/"`uname -r`"/kernel/drivers/intel/sgx" && sudo sh -c "cat /etc/modules | grep -Fxq isgx || echo isgx >> /etc/modules" && sudo /sbin/depmod && sudo /sbin/modprobe isgx && sudo service aesmd restart'
 ssh -t nuc@147.210.129.172 'cd ~/linux-sgx-driver/ && make clean && make && sudo mkdir -p "/lib/modules/"`uname -r`"/kernel/drivers/intel/sgx" && sudo cp isgx.ko "/lib/modules/"`uname -r`"/kernel/drivers/intel/sgx" && sudo sh -c "cat /etc/modules | grep -Fxq isgx || echo isgx >> /etc/modules" && sudo /sbin/depmod && sudo /sbin/modprobe isgx && sudo service aesmd restart'
