443425b0828425400072669cc93922d71507e83c
Simon Da Silva <contact@simondasilva.fr>
2018-06-25 17:22:25 +0200

    auto-addr

--- sgx-srdsserver/SGXSRDSEnclave/SGXSRDSEnclave.cpp
+++ sgx-srdsserver/SGXSRDSEnclave/SGXSRDSEnclave.cpp
@@ -15,9 +15,9 @@ bool encrypt = false;
 
 const std::string proxyPort("8081");
 const std::string proxyAddr = "localhost:" + proxyPort;
-const std::string trackerAddr("147.210.129.241:8888");
+const std::string trackerAddr("192.168.1.120:8888");
 const std::string serverAddr("localhost:8080");
-const std::string mpdAddr("147.210.128.146:8081");
+const std::string mpdAddr("192.168.1.109:8081");
 const std::string mpdRes = "/api/mpd/srds";
 
 int numberOfTokens = 4;
