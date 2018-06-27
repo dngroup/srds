659b4ae96670a46487d7d26be393e662f356e5ac
Simon Da Silva <contact@simondasilva.fr>
2018-06-27 18:15:28 +0200

    sleeps

--- sgx-srdsserver/exp-setup.sh
+++ sgx-srdsserver/exp-setup.sh
@@ -9,17 +9,20 @@ sleep 10
 
 ssh -o ProxyCommand="ssh -W %h:%p pi@$raspi" -f nuc@"$nuc1" '~/srds/sgx-srdsserver/start-content-server.sh'
 
-sleep 3
+sleep 10
 
 ssh -o ProxyCommand="ssh -W %h:%p pi@$raspi" -f nuc@"$nuc6" '~/srds/sgx-srdsserver/start-tracker.sh'
 
-sleep 3
+sleep 10
 
 command="~/srds/sgx-srdsserver/start-client.sh"
 
 ssh -o ProxyCommand="ssh -W %h:%p pi@$raspi" -f nuc@"$nuc2" "$command"
+sleep 10
 ssh -o ProxyCommand="ssh -W %h:%p pi@$raspi" -f nuc@"$nuc3" "$command"
+sleep 10
 ssh -o ProxyCommand="ssh -W %h:%p pi@$raspi" -f nuc@"$nuc4" "$command"
+sleep 10
 ssh -o ProxyCommand="ssh -W %h:%p pi@$raspi" -f nuc@"$nuc5" "$command"
 
 sleep 1000
