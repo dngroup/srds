c5f1a7d98e0c516211d4e8ba7b1ca4068811c856
Simon Da Silva <contact@simondasilva.fr>
2018-07-25 16:28:38 +0200

    Fast update

---
+++ sgx-srdsserver/exp-fast-update.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -x
+
+source exp-nucs-addr.sh
+source exp-stop.sh
+
+sleep 10
+
+command="cd ~/srds/sgx-srdsserver/ && git pull"
+
+ssh -o ProxyCommand="ssh -W %h:%p pi@$raspi" -f nuc@"$nuc1" "$command"
+ssh -o ProxyCommand="ssh -W %h:%p pi@$raspi" -f nuc@"$nuc2" "$command"
+ssh -o ProxyCommand="ssh -W %h:%p pi@$raspi" -f nuc@"$nuc3" "$command"
+ssh -o ProxyCommand="ssh -W %h:%p pi@$raspi" -f nuc@"$nuc4" "$command"
+ssh -o ProxyCommand="ssh -W %h:%p pi@$raspi" -f nuc@"$nuc5" "$command"
+ssh -o ProxyCommand="ssh -W %h:%p pi@$raspi" -f nuc@"$nuc6" "$command"
+
+sleep 60
+
