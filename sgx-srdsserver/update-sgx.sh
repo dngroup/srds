b7b98cb1182f59443b3c641f4aaf12fc9432a145
Simon Da Silva <contact@simondasilva.fr>
2018-08-16 18:18:32 +0200

    Update sgx

---
+++ sgx-srdsserver/update-sgx.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -x
+
+if [ -d "/home/nuc/Downloads/linux-sgx-driver/" ]; then
+  
+  cd /home/nuc/Downloads/linux-sgx-driver/
+  
+else
+  
+  cd /home/nuc/linux-sgx-driver/
+  
+fi
+
+git config user.email "toto"
+git config user.name "toto"
+git stash
+git pull
+make clean
+make
+sudo mkdir -p "/lib/modules/"`uname -r`"/kernel/drivers/intel/sgx"
+sudo cp isgx.ko "/lib/modules/"`uname -r`"/kernel/drivers/intel/sgx"
+sudo sh -c "cat /etc/modules | grep -Fxq isgx || echo isgx >> /etc/modules"
+sudo /sbin/depmod
+sudo depmod
+sudo /sbin/modprobe isgx
+sudo service aesmd restart
+
