54671ff064dba7b7e221f0e2d58647a6834caa62
toto <toto>
2018-08-22 12:27:58 +0200

    free?

--- sgx-srdsserver/SGXSRDSEnclave/SGXSRDSEnclave.cpp
+++ sgx-srdsserver/SGXSRDSEnclave/SGXSRDSEnclave.cpp
@@ -1018,10 +1018,10 @@ void handleProxy(int csock, char * msg, int msgsize) {
 		free(finalanswer);
 	}
 	if (answer != NULL) {
-		free(answer);
+		//free(answer);
 	}
 	if (target2 != NULL) {
-		//free(target2);
+		free(target2);
 	}
 }
 
