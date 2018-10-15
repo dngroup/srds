8bbef2f6bf03d1128d63d9bf2fbc2d771038dbfa
toto <toto>
2018-10-15 18:59:45 +0200

    Scripted bench

---
+++ sgx-srdsserver/exp-scripted-benchmark.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -x
+
+for i in {1..20}
+do
+   ./exp-benchmark.sh 1 1 60 "$i"
+done
+
+for i in {1..20}
+do
+   ./exp-benchmark.sh 2 2 60 "$i"
+done
+
+for i in {1..20}
+do
+   ./exp-benchmark.sh 2 3 60 "$i"
+done
+
+for i in {1..20}
+do
+   ./exp-benchmark.sh 2 4 60 "$i"
+done
+
+for i in {1..20}
+do
+   ./exp-benchmark.sh 3 3 60 "$i"
+done
+
+for i in {1..20}
+do
+   ./exp-benchmark.sh 3 4 60 "$i"
+done
+
+for i in {1..20}
+do
+   ./exp-benchmark.sh 4 4 60 "$i"
+done
+
