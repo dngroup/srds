e1f163129c0bce599aab19356b70e0bfbf8e91d9
toto <toto>
2018-10-16 15:36:04 +0200

    timeout

--- sgx-srdsserver/start-benchmark.sh
+++ sgx-srdsserver/start-benchmark.sh
@@ -28,7 +28,7 @@ sleep 10
 
 cd ~/wrk2
 
-./wrk -t"$1" -c"$2" -d"$3"s -R"$4" -s report.lua --timeout60s -H "Origin: http://localhost:8082" -H "Accept-Encoding: gzip, deflate, br" -H "X-Forwarded-Host: 5GGKUZXWKE2LVA5VMXRA5JAWJKHQE" -H "Accept-Language: en-US,en;q=0.9" -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/69.0.3497.81 Chrome/69.0.3497.81 Safari/537.36" -H "Accept: */*" -H "Referer: http://localhost:8082/samples/dash-if-reference-player/index.html" -H "Connection: keep-alive" "http://localhost:8081/api/description/srds/7?order=111111111111&q0=0&q1=4129849"
+./wrk -t"$1" -c"$2" -d"$3"s -R"$4" -s report.lua --timeout 60s -H "Origin: http://localhost:8082" -H "Accept-Encoding: gzip, deflate, br" -H "X-Forwarded-Host: 5GGKUZXWKE2LVA5VMXRA5JAWJKHQE" -H "Accept-Language: en-US,en;q=0.9" -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/69.0.3497.81 Chrome/69.0.3497.81 Safari/537.36" -H "Accept: */*" -H "Referer: http://localhost:8082/samples/dash-if-reference-player/index.html" -H "Connection: keep-alive" "http://localhost:8081/api/description/srds/7?order=111111111111&q0=0&q1=4129849"
 
 # chromium-browser --headless --remote-debugging-port=7777 --ignore-autoplay-restrictions --no-user-gesture-required --disable-default-apps --no-first-run --disk-cache-dir=/dev/null --disk-cache-size=1 http://localhost:8082/samples/dash-if-reference-player/index.html
 
