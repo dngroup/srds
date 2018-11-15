caec85e2bf5184176187784fb88685f940ff8fa3
Simon Da Silva <simon13.nomis09@gmail.com>
2018-11-15 18:20:52 +0100

    apply_profile

---
+++ sgx-srdsserver/apply_profile.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import csv
+import os
+
+with open('profiles.csv', newline='') as csvfile:
+    addr = 'localhost'
+    os.system('curl -X POST ' + addr + ':9000/api/upload/limit/1000000')
+    os.system('curl -X POST ' + addr + ':9000/api/download/limit/1000000')
+    reader = csv.DictReader(csvfile)
+    profiles = list(reader)
+    for value in profiles:
+        os.system('curl -X PUT ' + addr + ':9000/api/upload/limit/' + str(int(value['Bandwidth'])))
+        os.system('curl -X PUT ' + addr + ':9000/api/download/limit/' + str(int(value['Bandwidth'])))
+        os.system('curl -H \'Content-Type: application/json\' -X PUT -d \'{\"delay\":\"' + str(int(value['RTT'])/2) + '\", \"loss\":\"' + str(float(value['Loss'])) + '\"}\' ' + addr + ':9000/api/upload/netem')
+        os.system('curl -H \'Content-Type: application/json\' -X PUT -d \'{\"delay\":\"' + str(int(value['RTT'])/2) + '\", \"loss\":\"' + str(float(value['Loss'])) + '\"}\' ' + addr + ':9000/api/download/netem')
