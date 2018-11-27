1c5d455e7852b62ad5467659d1b6c3b7fd66ab63
toto <toto>
2018-11-27 14:59:41 +0100

    logserver

---
+++ sgx-srdsserver/logserver.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const fse = require('fs-extra');
+const shell = require('shelljs');
+const app = express();
+const jsonParser = bodyParser.json();
+
+app.use(express.static('public'));
+app.use(cors());
+
+const start = Date.now();
+
+shell.mkdir('-p', '/home/pi/logs/' + start);
+
+async function onSetWord(req, res) {
+  const routeParams = req.params;
+  const word = routeParams.word;
+  await fse.writeJson('/home/pi/logs/' + start + '/' + routeParams.word, req.body);
+  res.json({success: true});
+}
+app.post('/set/:word', jsonParser, onSetWord);
+
+app.listen(9999, function () {
+  console.log('Server listening on port 9999');
+});
+
