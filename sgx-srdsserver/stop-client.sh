8dd99a4f6d499dbc9992352aa2c4c343986cd8fe
toto <toto>
2019-01-17 11:20:00 +0100

    Auto-relaunch?

--- sgx-srdsserver/stop-client.sh
+++ sgx-srdsserver/stop-client.sh
@@ -2,6 +2,6 @@
 
 set -x
 
-killall firefox chromium-browser chrome google-chrome SGXSRDSServerApp firefox chromium-browser chrome google-chrome SGXSRDSServerApp node http-server gdb python3
+killall firefox chromium-browser chrome google-chrome SGXSRDSServerApp firefox chromium-browser chrome google-chrome SGXSRDSServerApp node http-server gdb python3 bash
 docker rm $(docker stop $(docker ps -a -q))
 
