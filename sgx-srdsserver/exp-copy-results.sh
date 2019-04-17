2bd1de40e52e6cb6657a735a86158f634f64d435
toto <toto>
2019-04-17 14:27:23 +0200

    exp1

--- sgx-srdsserver/exp-copy-results.sh
+++ sgx-srdsserver/exp-copy-results.sh
@@ -4,5 +4,5 @@ set -x
 
 source exp-nucs-addr.sh
 
-scp -o ProxyCommand="ssh -W %h:%p pi@$raspi" nuc@"$nuc4":/home/nuc/wrk2/logs/wrk.csv pi@"$raspi":~/wrk.csv && scp pi@"$raspi":~/wrk.csv /home/simon/Documents/SRDS2018/github/sgx-srdsserver/results/wrk.csv
+scp -o ProxyCommand="ssh -W %h:%p pi@$raspi" -r nuc@"$nuc4":/home/nuc/wrk2/logs /home/simon/Documents/SRDS2018/github/sgx-srdsserver/results/
 
