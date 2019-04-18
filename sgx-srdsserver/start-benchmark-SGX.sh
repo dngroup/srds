fd636259ffc11d21c19a8ced446e893284483a26
toto <toto>
2019-04-18 15:56:29 +0200

    sh

--- sgx-srdsserver/start-benchmark-SGX.sh
+++ sgx-srdsserver/start-benchmark-SGX.sh
@@ -22,9 +22,9 @@ cd ~/srds/sgx-srdsserver
 source /opt/intel/sgxsdk/environment
 make clean
 make
-(while sleep 1; do (~/srds/sgx-srdsserver/SGXSRDSServerApp.sh 8081 0); done) &
+(while sleep 1; do (~/srds/sgx-srdsserver/SGXSRDSServerApp 8081 0); done) &
 
-sleep 10
+sleep 5
 
 cd ~/wrk2
 
